b36f2970050942ca9f245b9e085d2e8a283e2b8d
Christina Liu <116124602+cyanliu@users.noreply.github.com>
2024-06-08 13:37:48 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>pikmin time</h1>
+        </header>
+      </div>
+      <div>
+        <div class="box"></div>
+      </div>
+    </>
   );
 }
 
