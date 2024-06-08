887cfa50b1b18212a082b94de076957eeb81149f
Christina Liu <116124602+cyanliu@users.noreply.github.com>
2024-06-08 14:56:37 -0400

    initial pass at parsing monthly challenges into components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,45 @@ import React from "react";
 import "./App.scss";
 import Stage from "./components/Stage";
 
+var bigOlThing = [
+  ["Walk 1000 steps"],
+  ["Grow 2 Pikmin"],
+  ["Complete 2 Expeditions"],
+  ["Plant 1000 Flowers", "Destroy 2 mushrooms"],
+  ["Walk 2000 Steps"],
+  ["Plant 1500 Flowers", "Destroy 2 mushrooms"],
+  ["Grow 3 Pikmin", "Destroy 3 Mushrooms"],
+  ["Plant 700 Peony", "Destroy 4 Mushrooms"],
+  ["Complete 3 Expeditions"],
+  ["Plant 1000 Red Calla Lily", "Destroy 3 Mushrooms"],
+  ["Plant 1500 Yellow Calla Lily", "Destroy 4 Mushrooms"],
+  ["Plant 2000 White Calla Lily", "Plant 1500 Peony", "Destroy 5 Mushrooms"],
+  ["Grow 3 Pikmin", "Destroy 3 Mushrooms"],
+  ["Plant 2000 White Peony", "Walk 3000 Steps", "Destroy 3 Mushrooms"],
+  [
+    "Plant 2000 Yellow Peony",
+    "Plant 1000 Blue Calla Lily",
+    "Destroy 4 Mushrooms",
+  ],
+  ["Plant 2000 Blue Peony", "Plant 2000 Calla Lily", "Destroy 5 Mushrooms"],
+];
+export type MyObject = { [key: string]: String[] };
+var bigOlObj: MyObject = {};
+var stageIncrement = 1;
+var miniStep = 1;
+for (let i = 1; i <= bigOlThing.length; i++) {
+  let newKey = stageIncrement + "." + miniStep;
+  bigOlObj[newKey] = bigOlThing[i - 1];
+
+  miniStep += 1;
+  if (i % 4 === 0) {
+    stageIncrement += 1;
+    miniStep = 1;
+  }
+}
+
+console.log(bigOlObj);
+
 function App() {
   return (
     <>
@@ -12,10 +51,10 @@ function App() {
       </div>
       <div className="body">
         <div className="stage-container">
-          <Stage stageNum={1}></Stage>
-          <Stage stageNum={2}></Stage>
+          <Stage stageNum={1} stageGoals={bigOlObj}></Stage>
+          {/* <Stage stageNum={2}></Stage>
           <Stage stageNum={3}></Stage>
-          <Stage stageNum={4}></Stage>
+          <Stage stageNum={4}></Stage> */}
         </div>
       </div>
     </>
--- src/components/Stage.tsx
+++ src/components/Stage.tsx
@@ -1,7 +1,17 @@
 import React, { ReactNode, useState } from "react";
+import { MyObject } from "../App";
 
 type StageProps = {
   stageNum: number;
+  stageGoals: MyObject;
+};
+
+var test = {
+  "1.1": ["Walk 1000 steps"],
+  "1.2": ["Grow 2 Pikmin"],
+  "1.3": ["Complete 2 Expeditions"],
+  "1.4": ["Plant 1000 Flowers", "Destroy 2 mushrooms"],
+  "2.1": ["something else"],
 };
 
 export default function Stage({ stageNum }: StageProps) {
